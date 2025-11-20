12ab36e21595746325102b9c44e8dbc344d14265
anth0nycodes <hoanganthony2207@gmail.com>
2025-11-20 02:46:50 -0500

    remove the X days ago from blog dates

--- components/blog-date.tsx
+++ components/blog-date.tsx
@@ -2,10 +2,7 @@ import { formatDate } from "@/lib/utils";
 
 export function BlogDate({ date }: { date: string }) {
   return (
-    <p
-      className="text-sm text-neutral-600 dark:text-neutral-400"
-      suppressHydrationWarning
-    >
+    <p className="text-sm text-neutral-600 dark:text-neutral-400">
       {formatDate(date)}
     </p>
   );
