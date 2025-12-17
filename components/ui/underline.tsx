01aaec0aa3506675a6d6f088fa05b800b070a5f9
anth0nycodes <hoanganthony2207@gmail.com>
2025-12-16 19:42:26 -0500

    added confetti when clicking on underlines

--- components/ui/underline.tsx
+++ components/ui/underline.tsx
@@ -1,6 +1,8 @@
 "use client";
+
 import { motion } from "framer-motion";
-import { ReactNode } from "react";
+import { ReactNode, useRef } from "react";
+import Realistic from "react-canvas-confetti/dist/presets/realistic";
 
 interface UnderlineProps {
   hexcode: string;
@@ -15,19 +17,37 @@ export function Underline({
   duration,
   children,
 }: UnderlineProps) {
+  const controller = useRef<{ shoot: () => void } | null>(null);
+
+  const onInitHandler = ({
+    conductor,
+  }: {
+    conductor: { shoot: () => void };
+  }) => {
+    controller.current = conductor;
+  };
+
+  const onShoot = () => {
+    controller.current?.shoot();
+  };
+
   return (
-    <motion.span
-      className="inline bg-no-repeat pb-0.5"
-      initial={{ backgroundSize: "0% 3px" }}
-      whileInView={{ backgroundSize: "100% 3px" }}
-      viewport={{ once: true }}
-      transition={{ delay: delay, duration: duration, ease: "easeInOut" }}
-      style={{
-        backgroundImage: `linear-gradient(${hexcode}, ${hexcode})`,
-        backgroundPosition: "0 calc(100% - 1px)",
-      }}
-    >
-      {children}
-    </motion.span>
+    <>
+      <Realistic onInit={onInitHandler} />
+      <motion.span
+        onClick={onShoot}
+        className="inline cursor-pointer bg-no-repeat pb-0.5"
+        initial={{ backgroundSize: "0% 3px" }}
+        whileInView={{ backgroundSize: "100% 3px" }}
+        viewport={{ once: true }}
+        transition={{ delay: delay, duration: duration, ease: "easeInOut" }}
+        style={{
+          backgroundImage: `linear-gradient(${hexcode}, ${hexcode})`,
+          backgroundPosition: "0 calc(100% - 1px)",
+        }}
+      >
+        {children}
+      </motion.span>
+    </>
   );
 }
